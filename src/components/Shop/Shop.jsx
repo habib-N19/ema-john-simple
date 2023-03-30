0e7bf191f3185b20a92f1af83f5ec8e8f7725ae5
habib-N19 <habiburnabiarafat@gmail.com>
2023-03-30 23:59:16 +0600

    retrive cart data from local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { addToDb, getShoppingCart } from '../../utilities/fakedb'
 import Cart from '../Cart/Cart'
+import Products from '../Product/Product'
 import Product from '../Product/Product'
 import './Shop.css'
 
@@ -15,9 +16,35 @@ const Shop = () => {
   }, [])
   useEffect(() => {
     const storedCart = getShoppingCart()
-  }, [])
+    const savedCart = []
+    // step 1 : get the id
+    for (const id in storedCart) {
+      // step 2: get the product by using id
+      const addedProduct = products.find(product => product.id === id)
+      if (addedProduct) {
+        // step 3: get the quantity of the product
+
+        const quantity = storedCart[id]
+        addedProduct.quantity = quantity
+        // step 4: add the added product to the saved cart
+        savedCart.push(addedProduct)
+      }
+    }
+    // step 5: set the cart
+    setCart(savedCart)
+  }, [products])
   const handleAddToCart = product => {
-    const newCart = [...cart, product]
+    // const newCart = [...cart, product]
+    let newCart = []
+    const exists = cart.find(pd => pd.id === product.id)
+    if (!exists) {
+      product.quantity = 1
+      newCart = [...cart, product]
+    } else {
+      exists.quantity = exists.quantity + 1
+      const remaining = cart.filter(pd => pd.id !== product.id)
+      newCart = [...remaining, exists]
+    }
     setCart(newCart)
     addToDb(product.id)
   }
